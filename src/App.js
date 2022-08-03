0c9a35b8b419d5a9df23f1ebd649ea624bba4f83
gerobatest <108824541+gerobatest@users.noreply.github.com>
2022-08-03 10:27:42 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import Carousel from "./components/Carousel";
-import APropos from "./components/APropos";
-import Expertise from "./components/Expertise";
-import Innovation from "./components/Innovation";
-import Actualite from "./components/Actualite";
-import Collaborations from "./components/Collaborations";
-import Footer from "./components/Footer";
-import { ParallaxProvider } from 'react-scroll-parallax';
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      {/**/}{/**/}
-      <Carousel/>
-      <APropos/>
-      <Expertise/>
-      <ParallaxProvider> 
-        <Innovation/> 
-      </ParallaxProvider>
-      <Actualite/>
-      {/**/}<Collaborations/> 
-      <Footer/>
+      <Navbar />
     </>
   );
 }
-
 export default App;
